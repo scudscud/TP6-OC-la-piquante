ef058036e38d9a80d960e000e412e6e0d98710b5
scud <guillaume.scudetti@gmail.com>
2022-07-03 18:25:23 +0200

    imageok

--- TP6- la piquante/backend/controllers/sauces.js
+++ TP6- la piquante/backend/controllers/sauces.js
@@ -5,7 +5,7 @@ exports.createSauce = (req, res, next) => {
   delete sauceObject._id;
   delete sauceObject._userId;
 
-  const sauce = new Sauce({ ...sauceObject, userId: req.auth.userId, imageUrl: `${req.protocol}://${req.get('host')}/image/${req.file.filename}` });
+  const sauce = new Sauce({ ...sauceObject, userId: req.auth.userId, imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}` });
   sauce.save()
     .then(() => {
       res.status(201).json({ message: "ajout reussit" });
